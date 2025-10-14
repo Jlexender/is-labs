0f3bc3c2d63e57c5cb3485cb86e767c601269d58
Alexander Kaliev <aleksandrdmitryevich@gmail.com>
2025-10-14 22:45:27 +0300

    cleanup vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home">
+        <h1>Home View</h1>
+        <p>Welcome to the Home View!</p>
+    </div>
 </template>
+
+<style scoped>
+</style>
